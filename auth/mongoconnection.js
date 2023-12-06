d7dd1ef4e78f8f3a06c7fd912c07a2dbf3f1442b
Jeevan Kumar Konduru <jeevankumarkonduru@Jeevans-MacBook-Pro.local>
2023-12-05 21:28:19 -0500

    first commit

---
+++ auth/mongoconnection.js
@@ -0,0 +1,6 @@
+module.exports = {
+    connectionurl: 'mongodb+srv://000jeevankumar:Kjk9510!!!@taskappcluster.8li99mk.mongodb.net/',
+    database: 'TaskDatabase',
+    collection: 'TaskCollection'
+  };
+  
